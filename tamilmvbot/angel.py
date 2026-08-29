6b15836ff9b0881ca515e3b893bd0b3c590f043c
harsh <harshad107@gmail.com>
2026-08-29 23:35:24 +0530

    Fix Vercel SPA routing

--- tamilmvbot/angel.py
+++ tamilmvbot/angel.py
@@ -122,11 +122,11 @@ def tamilmv():
  
         temps = soup.find_all('div', {'class': 'ipsType_break ipsContained'}) 
  
-        if len(temps) < 100: 
+        if len(temps) < 50: 
             logger.warning("Not enough movies found on the page") 
             return [], {} 
  
-        for i in range(100): 
+        for i in range(50): 
             title = temps[i].findAll('a')[0].text.strip() 
             link = temps[i].find('a')['href'] 
             movie_list.append(title) 
